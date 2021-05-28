049834d82616d4c98c5f9dee4ae6def1b9d70d86
Mauro Parafati <mparafati@fi.uba.ar>
2021-05-28 02:50:42 -0300

    [MOD] Hidden topbar and custom title

--- src/routes/docsRouter.js
+++ src/routes/docsRouter.js
@@ -2,8 +2,17 @@ const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 
 module.exports = function docsRouter(apiSpecs) {
+  const SwaggerOptions = {
+    customCss: `
+    .swagger-ui .topbar {
+      display: none;
+    }`,
+
+    customSiteTitle: 'Seedy FIUBA API'
+  };
+
   return express
     .Router()
     .use('/api-docs', swaggerUi.serve)
-    .get('/api-docs', swaggerUi.setup(apiSpecs));
+    .get('/api-docs', swaggerUi.setup(apiSpecs, SwaggerOptions));
 };
