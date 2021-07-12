9fe8fde0aeb055c270a7187cf35b807fce0e6634
Mauro Parafati <mparafati@fi.uba.ar>
2021-07-12 03:52:24 -0300

    [ADD] Reviewers

--- src/gateways/usersGateway.js
+++ src/gateways/usersGateway.js
@@ -1,7 +1,16 @@
 module.exports = function $usersGateway(fetch, services, urlFactory) {
   return {
+    getNames,
     login
   };
+
+  function getNames(userIds) {
+    const url = urlFactory('/names', services.users.baseUrl);
+
+    return fetch(url, { method: 'POST', body: userIds }).then(
+      ({ data }) => data
+    );
+  }
 
   function login(credentials, type) {
     const path = type === 'USER' ? '/users/session' : '/admins/session';
