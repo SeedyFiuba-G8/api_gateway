a68efacc5fbc20173db77081b0bd37bef3f09ba1
sankle <santi.klein@hotmail.com>
2021-07-27 19:43:47 -0300

    [ADD] postWallet in creating user with fbToken

--- src/controllers/sessionController.js
+++ src/controllers/sessionController.js
@@ -1,4 +1,13 @@
-module.exports = function $sessionController(expressify, sessionService) {
+module.exports = function $sessionController(
+  apikeys,
+  apikeyUtils,
+  expressify,
+  forwardingService,
+  logger,
+  services,
+  sessionService,
+  urlFactory
+) {
   return expressify({
     loginAdmin,
     loginUser
@@ -17,6 +26,28 @@ module.exports = function $sessionController(expressify, sessionService) {
   async function login(req, res, type) {
     const credentials = req.body;
     const session = await sessionService.login(credentials, type);
+
+    if (credentials.fbToken && type === 'USER') {
+      const { core: coreApikey } = await apikeys;
+      const postWalletUrl = urlFactory('wallets', services.core.baseUrl);
+      try {
+        const postWalletRes = await forwardingService.forward(
+          {},
+          {
+            body: { uid: session.id },
+            method: 'POST',
+            url: postWalletUrl,
+            headers: apikeyUtils.headers(coreApikey)
+          }
+        );
+        logger.info(
+          `Wallet address ${postWalletRes.data} created for user ${session.id}`
+        );
+      } catch (err) {
+        // Wallet already exists
+        return res.status(200).json(session);
+      }
+    }
 
     return res.status(200).json(session);
   }
