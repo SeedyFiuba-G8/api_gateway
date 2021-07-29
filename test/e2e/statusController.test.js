64e8529828883bb8380132c7ca0d5ad7b104dbbd
TaielC <43725160+TaielC@users.noreply.github.com>
2021-07-29 00:11:18 -0300

    completed statusController tests

--- test/e2e/statusController.test.js
+++ test/e2e/statusController.test.js
@@ -15,11 +15,19 @@ describe('statusController', () => {
       health: {
         database: 'UP'
       },
+      info: {
+        creationDate: '2021-06-01T01:00:00.000Z',
+        description: 'Microservice description.'
+      },
       ping: 'ok'
     },
     core: {
       health: {
         database: 'UP'
+      },
+      info: {
+        creationDate: '2021-06-01T01:00:00.000Z',
+        description: 'Microservice description.'
       },
       ping: 'ok'
     },
@@ -27,11 +35,19 @@ describe('statusController', () => {
       health: {
         database: 'UP'
       },
+      info: {
+        creationDate: '2021-06-01T01:00:00.000Z',
+        description: 'Microservice description.'
+      },
       ping: 'ok'
     },
     users: {
       health: {
         database: 'UP'
+      },
+      info: {
+        creationDate: '2021-06-01T01:00:00.000Z',
+        description: 'Microservice description.'
       },
       ping: 'ok'
     }
@@ -49,6 +65,11 @@ describe('statusController', () => {
           badStatus: () => statusNocks.nockApiKeysHealth(500),
           timeout: () => statusNocks.nockApiKeysTimeout('/health')
         },
+        info: {
+          ok: () => statusNocks.nockApiKeysInfo(),
+          badStatus: () => statusNocks.nockApiKeysInfo(500),
+          timeout: () => statusNocks.nockApiKeysTimeout('/info')
+        },
         ping: {
           ok: () => statusNocks.nockApiKeysPing(),
           badStatus: () => statusNocks.nockApiKeysPing(500),
@@ -60,6 +81,11 @@ describe('statusController', () => {
           ok: () => statusNocks.nockCoreHealth(),
           badStatus: () => statusNocks.nockCoreHealth(500),
           timeout: () => statusNocks.nockCoreTimeout('/health')
+        },
+        info: {
+          ok: () => statusNocks.nockCoreInfo(),
+          badStatus: () => statusNocks.nockCoreInfo(500),
+          timeout: () => statusNocks.nockCoreTimeout('/info')
         },
         ping: {
           ok: () => statusNocks.nockCorePing(),
@@ -73,6 +99,11 @@ describe('statusController', () => {
           badStatus: () => statusNocks.nockSCHealth(500),
           timeout: () => statusNocks.nockSCTimeout('/health')
         },
+        info: {
+          ok: () => statusNocks.nockSCInfo(),
+          badStatus: () => statusNocks.nockSCInfo(500),
+          timeout: () => statusNocks.nockSCTimeout('/info')
+        },
         ping: {
           ok: () => statusNocks.nockSCPing(),
           badStatus: () => statusNocks.nockSCPing(500),
@@ -84,6 +115,11 @@ describe('statusController', () => {
           ok: () => statusNocks.nockUsersHealth(),
           badStatus: () => statusNocks.nockUsersHealth(500),
           timeout: () => statusNocks.nockUsersTimeout('/health')
+        },
+        info: {
+          ok: () => statusNocks.nockUsersInfo(),
+          badStatus: () => statusNocks.nockUsersInfo(500),
+          timeout: () => statusNocks.nockUsersTimeout('/info')
         },
         ping: {
           ok: () => statusNocks.nockUsersPing(),
@@ -135,6 +171,65 @@ describe('statusController', () => {
             beforeEach(() => {
               response.services[service] = 'bad status';
               nockFuncs[service].health.badStatus();
+            });
+
+            it('should respond with correct status and body', () =>
+              request
+                .get(path)
+                .expect('Content-Type', /json/)
+                .expect(200, response));
+          });
+        });
+      });
+
+      describe('when all services are UP', () => {
+        it('should respond with correct status and body', () =>
+          request
+            .get(path)
+            .expect('Content-Type', /json/)
+            .expect(200, response));
+      });
+    });
+  });
+
+  describe('/info', () => {
+    const path = '/info';
+
+    describe('GET', () => {
+      let response;
+
+      beforeEach(() => {
+        response = {
+          apigateway: {
+            creationDate: '2021-06-01T01:00:00.000Z',
+            description: 'Microservice description.'
+          }
+        };
+        services.forEach((service) => {
+          response[service] = expected[service].info;
+          nockFuncs[service].info.ok();
+        });
+      });
+
+      services.forEach((service) => {
+        describe(`when ${service} is down`, () => {
+          describe(`when ${service} times out`, () => {
+            beforeEach(() => {
+              response[service] = 'timed out';
+              nockFuncs[service].info.timeout();
+            });
+
+            it('should respond with correct status and body', () =>
+              request
+                .get(path)
+                .expect('Content-Type', /json/)
+                .expect(200, response));
+          });
+
+          describe(`when ${service} returns bad status`, () => {
+            beforeEach(() => {
+              response[service] = 'bad status';
+              nockFuncs[service].info.badStatus();
             });
 
             it('should respond with correct status and body', () =>
--- test/support/statusNocks.js
+++ test/support/statusNocks.js
@@ -3,6 +3,8 @@ module.exports = function $statusNocks(axiosMock, services, urlFactory) {
     // API KEYS
     nockApiKeysHealth: (statusCode) =>
       nockHealth(services.apikeys.baseUrl, { statusCode }),
+    nockApiKeysInfo: (statusCode) =>
+      nockInfo(services.apikeys.baseUrl, { statusCode }),
     nockApiKeysPing: (statusCode) =>
       nockPing(services.apikeys.baseUrl, { statusCode }),
     nockApiKeysTimeout: (path) =>
@@ -11,6 +13,8 @@ module.exports = function $statusNocks(axiosMock, services, urlFactory) {
     // CORE
     nockCoreHealth: (statusCode) =>
       nockHealth(services.core.baseUrl, { statusCode }),
+    nockCoreInfo: (statusCode) =>
+      nockInfo(services.core.baseUrl, { statusCode }),
     nockCorePing: (statusCode) =>
       nockPing(services.core.baseUrl, { statusCode }),
     nockCoreTimeout: (path) => nockGetTimeout(path, services.core.baseUrl),
@@ -18,12 +22,16 @@ module.exports = function $statusNocks(axiosMock, services, urlFactory) {
     // SC
     nockSCHealth: (statusCode) =>
       nockHealth(services.sc.baseUrl, { statusCode }),
+    nockSCInfo: (statusCode) =>
+      nockInfo(services.sc.baseUrl, { statusCode }),
     nockSCPing: (statusCode) => nockPing(services.sc.baseUrl, { statusCode }),
     nockSCTimeout: (path) => nockGetTimeout(path, services.sc.baseUrl),
 
     // USERS
     nockUsersHealth: (statusCode) =>
       nockHealth(services.users.baseUrl, { statusCode }),
+    nockUsersInfo: (statusCode) =>
+      nockInfo(services.users.baseUrl, { statusCode }),
     nockUsersPing: (statusCode) =>
       nockPing(services.users.baseUrl, { statusCode }),
     nockUsersTimeout: (path) => nockGetTimeout(path, services.users.baseUrl)
@@ -35,6 +43,19 @@ module.exports = function $statusNocks(axiosMock, services, urlFactory) {
     { statusCode = 200, data = { database: 'UP' } } = {}
   ) {
     return nockGet('/health', baseUrl, { statusCode, data });
+  }
+
+  function nockInfo(
+    baseUrl,
+    {
+      statusCode = 200,
+      data = {
+        creationDate: '2021-06-01T01:00:00.000Z',
+        description: 'Microservice description.'
+      }
+    }
+  ) {
+    return nockGet('/info', baseUrl, { statusCode, data });
   }
 
   function nockPing(
