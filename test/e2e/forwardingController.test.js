d69b9d7bc115f9c919ef8f17933ea6ae88ea3354
TaielC <43725160+TaielC@users.noreply.github.com>
2021-08-02 03:41:42 -0300

    Added test for projects and users Controllers, 92%

--- test/e2e/forwardingController.test.js
+++ test/e2e/forwardingController.test.js
@@ -96,10 +96,6 @@ describe('forwardingController', () => {
 
     beforeEach(() => {
       baseNocks.users.patch({ path, params: patchBody }, () => [200]);
-      axiosMock.onAny().reply((config) => {
-        console.log('unhandled: ', config.method, config.url);
-        return [404];
-      });
     });
 
     describe('PATCH', () => {
@@ -120,10 +116,6 @@ describe('forwardingController', () => {
     beforeEach(() => {
       baseNocks.users.post({ path }, () => [204]);
       baseNocks.users.delete({ path }, () => [204]);
-      axiosMock.onAny().reply((config) => {
-        console.log('unhandled: ', config.method, config.url);
-        return [404];
-      });
     });
 
     describe('POST', () => {
@@ -148,4 +140,80 @@ describe('forwardingController', () => {
   });
 });
 
-describe('forwardingController.core', () => {});
+describe('forwardingController.core', () => {
+  let request;
+  let spyjwt;
+  let axiosMock;
+  let baseNocks;
+
+  beforeEach(() => {
+    spyjwt = {};
+    request = supertest(container.get('app'));
+    axiosMock = container.get('axiosMock');
+    baseNocks = container.get('baseNocks');
+
+    spyjwt.sign = jest.spyOn(jwt, 'sign').mockImplementation(({ type }) => {
+      if (type === 'USER') return fakeUserToken;
+      return fakeAdminToken;
+    });
+    spyjwt.verify = jest
+      .spyOn(jwt, 'verify')
+      .mockImplementation(async (token) => {
+        switch (token) {
+          case fakeUserToken: {
+            return { id: fakeId, type: 'USER' };
+          }
+          case fakeAdminToken: {
+            return { id: fakeId, type: 'ADMIN' };
+          }
+          default:
+            throw Error({ name: 'JsonWebTokenError' });
+        }
+      });
+  });
+
+  afterEach(() => {
+    axiosMock.reset();
+    jest.clearAllMocks();
+  });
+  
+  describe('/users/:userId/fundings', () => {
+    const path = `/users/${fakeId}/fundings`;
+    const transactionList = [
+      {
+        userId: '123e4567-e89b-12d3-a456-426614174000',
+        txHash:
+          '0x30b003c570eccaf1705acd4621f72993acb51715f8decbf61535f21376cfe1d2',
+        projectId: '123e4567-e89b-12d3-a456-426614174000',
+        amount: 0.000001,
+        date: '2021-06-13T21:29:29.330Z'
+      }
+    ];
+
+    beforeEach(() => {
+      baseNocks.core.get({ path }, () => [200, transactionList]);
+    });
+
+    describe('GET', () => {
+      describe('when a user wants to get their sent fundings', () => {
+        it('should return with status 200 and a list of transactions', () =>
+          request
+            .get(path)
+            .set(bearerAuthHeader, `Bearer ${fakeUserToken}`)
+            .expect(200, transactionList));
+      });
+    })
+    
+  });
+
+// '/users/fundings'
+// '/projects/:projectId'
+// '/projects/:projectId/funds'
+// '/projects/:projectId/block'
+// '/projects/:projectId/block'
+// '/projects/:projectId/like'
+// '/projects/:projectId/like'
+// '/projects/:projectId/rating'
+// '/reviewrequests/:reviewerId'
+// '/reviewrequests/:reviewerId/:projectId'
+});
---
+++ test/e2e/userController.test.js
@@ -0,0 +1,152 @@
+const _ = require('lodash');
+const supertest = require('supertest');
+const jwt = require('jsonwebtoken');
+
+const containerFactory = require('../testContainerFactory');
+
+const container = containerFactory.createContainer();
+const fakeUserToken = 'fake-user-token';
+const fakeAdminToken = 'fake-admin-token';
+const bearerAuthHeader = 'Authorization';
+const fakeId = '00000000-feed-0000-0000-c0ffee000000';
+
+describe('userController', () => {
+  let request;
+  let spyjwt;
+  let axiosMock;
+  let baseNocks;
+
+  beforeEach(() => {
+    spyjwt = {};
+    request = supertest(container.get('app'));
+    axiosMock = container.get('axiosMock');
+    baseNocks = container.get('baseNocks');
+
+    spyjwt.sign = jest.spyOn(jwt, 'sign').mockImplementation(({ type }) => {
+      if (type === 'USER') return fakeUserToken;
+      return fakeAdminToken;
+    });
+    spyjwt.verify = jest
+      .spyOn(jwt, 'verify')
+      .mockImplementation(async (token) => {
+        switch (token) {
+          case fakeUserToken: {
+            return { id: fakeId, type: 'USER' };
+          }
+          case fakeAdminToken: {
+            return { id: fakeId, type: 'ADMIN' };
+          }
+          default:
+            throw Error({ name: 'JsonWebTokenError' });
+        }
+      });
+  });
+
+  afterEach(() => {
+    axiosMock.reset();
+    jest.clearAllMocks();
+  });
+
+  describe('/users', () => {
+    const path = '/users';
+    const userInfo = {
+      email: 'test@test.com',
+      password: 'testpasswd',
+      firstName: 'Test',
+      lastName: 'User'
+    };
+
+    beforeEach(() => {
+      baseNocks.users.post({ path }, () => [
+        201,
+        { id: fakeId, ..._.omit(userInfo, ['password']) }
+      ]);
+      baseNocks.core.post({ path: '/wallets', params: { uid: fakeId } }, () => [
+        201,
+        '0xf018Be4Fe4fBD4cA1B1162A44bB139a343C2087b'
+      ]);
+    });
+
+    describe('POST', () => {
+      describe('when a new user wants to be registered', () => {
+        it('should return success code 200 and their information', () =>
+          request.post(path).send(userInfo).expect(201, { id: fakeId }));
+      });
+    });
+  });
+
+  describe('/users/:userId', () => {
+    const path = `/users/${fakeId}`;
+    const userInfo = {
+      firstName: 'Memis',
+      lastName: 'Pomofot',
+      banned: false,
+      signupDate: '2021-06-13T21:29:29.330Z',
+      city: 'Buenos Aires',
+      country: 'Argentina',
+      interests: ['music', 'entertainment', 'productivity']
+    };
+    const fullUserInfo = {
+      firstName: 'Memis',
+      lastName: 'Pomofot',
+      banned: false,
+      signupDate: '2021-06-13T21:29:29.330Z',
+      city: 'Buenos Aires',
+      country: 'Argentina',
+      interests: ['music', 'entertainment', 'productivity'],
+      address: '0xf018Be4Fe4fBD4cA1B1162A44bB139a343C2087b',
+      balance: 0.000001
+    };
+
+    beforeEach(() => {
+      baseNocks.users.get({ path }, () => [200, userInfo]);
+      baseNocks.core.get({ path: `/wallets/${fakeId}` }, () => [
+        200,
+        {
+          address: '0xf018Be4Fe4fBD4cA1B1162A44bB139a343C2087b',
+          balance: 0.000001
+        }
+      ]);
+    });
+
+    describe('GET', () => {
+      describe('when a user wants to get another´s profile', () => {
+        it('should return success code 200 and their information', () =>
+          request
+            .get(path)
+            .set(bearerAuthHeader, `Bearer ${fakeUserToken}`)
+            .expect(200, fullUserInfo));
+      });
+    });
+  });
+
+  describe('/users/:userId/message', () => {
+    const path = `/users/${fakeId}/message`;
+    const userInfo = {
+      email: 'test@test.com',
+      password: 'testpasswd',
+      firstName: 'Test',
+      lastName: 'User'
+    };
+
+    beforeEach(() => {
+      baseNocks.users.get({ path: `/users/${fakeId}` }, () => [200, userInfo]);
+      baseNocks.core.post({ path }, () => [200, fakeId]);
+      axiosMock.onAny().reply((config) => {
+        console.log('unhandled', config.method, config.url);
+        return [404];
+      });
+    });
+
+    describe('POST', () => {
+      describe('when a user wants to send a push message notification to another', () => {
+        it('should return with code 200 and the receivers id', () =>
+          request
+            .post(path)
+            .set(bearerAuthHeader, `Bearer ${fakeUserToken}`)
+            .send({ message: 'This is an example message.' })
+            .expect(200, `"${fakeId}"`));
+      });
+    });
+  });
+});
